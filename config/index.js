fd4c1f79297f2bfae334bc0d4498a4517dc93709
Ethan <372513646@qq.com>
2019-01-08 16:38:43 +0800

    update config/index

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/spa/',
+    assetsPublicPath: '/more/',
 
     /**
      * Source Maps
